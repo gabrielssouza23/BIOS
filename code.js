c2e3dafcd1d353c7f4f8020bc24046e1a420bf31
Enzo da Silva Coronel Garcia <86988685+EnzoCoronel@users.noreply.github.com>
2022-11-29 10:26:24 -0300

    mid_changes

--- code.js
+++ code.js
@@ -1,6 +1,15 @@
 function openNav() {
-  var x = document.getElementById("nav");
-  x.style.width = "300px";
+  let x = document.getElementById("nav");
+  let width =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+  if (width > "425px") {
+    x.style.height = "100px";
+    console.log("Socorro");
+  } else {
+    x.style.width = "300px";
+  }
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
@@ -8,4 +17,14 @@ function openNav() {
 function closeNav() {
   document.getElementById("nav").style.width = "0";
   document.body.style.backgroundColor = "white";
+}
+
+function more(id) {
+  var requests = document.getElementsByClassName("demand");
+  var i;
+  for (i = 0; i < requests.length; i++) {
+    var openRequest = requests[i];
+    openRequest.style.height = "6.5rem";
+  }
+  document.getElementById(id).style.height = "auto";
 }
