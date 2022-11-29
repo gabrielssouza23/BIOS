2cd8911f0e14f15a4e6ae51c4495c60e00c9df9c
Enzo da Silva Coronel Garcia <86988685+EnzoCoronel@users.noreply.github.com>
2022-11-29 04:53:58 -0300

    organize

---
+++ source/registerRedirecionamento.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styleRegister.css">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="shortcut icon" href="./icon/favicon.ico" type="image/x-icon">
+    <title>Bee.co</title>
+</head>
+<body>
+    <header>
+        <img src="./img/Beeco-removebg-preview.png" alt="">
+    </header>
+    <div id="reg">
+        <p id="paragrafo">Qual o tipo de usuário é você?</p>
+        <br>
+        <div id="links">
+            <a href="./registerEmpresa.html" id="empresario"><img src="./img/empres-removebg-preview.png" ></a>
+            
+            <a href="./registerContratante.html" id="contratante"><img src="./img/contra-removebg-preview.png" alt=""></a>
+        </div>
+    </div>
+        <footer></footer>
+</body>
+</html>
